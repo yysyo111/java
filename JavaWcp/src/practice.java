b61c307780c59f6afcd3d799b348111cd04a38bb
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-23 23:39:00 +0900

    [add]

--- JavaWcp/src/practice.java
+++ JavaWcp/src/practice.java
@@ -6,5 +6,12 @@ public class practice {
 		System.out.println("お酒が好きです");
 		System.out.println("31 + 31の計算をします");
 		System.out.println(31 + 31);
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println("答えは" + 64);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
 	}
 }
