bd513df42188f5d5aae85042e34dce4c56cd32ad
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-25 15:04:27 +0900

    [add]抽象クラスの継承

--- JavaWcp/src/extend/human/Human.java
+++ JavaWcp/src/extend/human/Human.java
@@ -1,7 +1,9 @@
 package extend.human;
 
 public class Human {
+	// privateは自分自身からのみアクセス可能
 	private String name;
+	// protectedはサブクラスからもアクセス可能
 	protected int age;
 	
 	public Human(String name, int age) {
