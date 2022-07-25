281a43244ef4845d14e82c1d716adde7544ea1b1
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-25 14:38:29 +0900

    [add]

--- JavaWcp/src/extend/human/Student.java
+++ JavaWcp/src/extend/human/Student.java
@@ -1,14 +1,19 @@
 package extend.human;
 
+//Humanクラスを継承したStudentクラス
 public class Student extends Human {
+	// サブクラスで追加したフィールド
 	private int score;
 	
 	public Student(String name, int age, int score) {
+		// スーパークラスのコンストラクタを呼び出し
 		super(name, age);
 		this.score = score;
 	}
 	
+	// サブクラスで追加したメソッド
 	public String getStudentProfile() {
+		// 親インスタンスのフィールドをsuper変数で参照
 		String profile = "年齢は" + super.age + "です";
 		profile += "学生で、でテストの点数は" + this.score + "点です";
 		return profile;
