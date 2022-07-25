14f964a99d551e9263b9a649a128a920e914de19
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-25 23:59:25 +0900

    [add]復習

---
+++ JavaWcp/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+//		変数宣言の文(ageという箱を用意)
+		int age;
+//		箱に数字の「30」を入れる
+		age = 30;
+//		箱の中身を表示
+		System.out.println(age);
+	}
+}
+
+//変数宣言の文
+//型 変数名;
