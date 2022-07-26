7b9705383cf13ba162ba2f8996918fdd67b929d0
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-26 23:18:33 +0900

    [add]練習

--- JavaWcp/src/Main01.java
+++ JavaWcp/src/Main01.java
@@ -1,20 +1,30 @@
+//
+//public class Main01 {
+//	public static void main(String[] args) {
+//		int a = 3;
+//		int b = 5;
+//		int c = a * b;
+//		System.out.println("縦幅3横幅3の長方形の面積は" + c);
+//		
+//		boolean result = true;
+//		System.out.println(result);
+//		char favoriteCharacter = '駆';
+//		System.out.println(favoriteCharacter);
+//		double pi = 3.14;
+//		System.out.println(pi);
+//		long number = 3141592655853979L;
+//		System.out.println(number);
+//		String msg = "ミナトの攻撃！的に15ポイントのダメージを与えた";
+//		System.out.println(msg);
+//	}
+//}
+
 
 public class Main01 {
 	public static void main(String[] args) {
-		int a = 3;
-		int b = 5;
-		int c = a * b;
-		System.out.println("縦幅3横幅3の長方形の面積は" + c);
-		
-		boolean result = true;
-		System.out.println(result);
-		char favoriteCharacter = '駆';
-		System.out.println(favoriteCharacter);
-		double pi = 3.14;
-		System.out.println(pi);
-		long number = 3141592655853979L;
-		System.out.println(number);
-		String msg = "ミナトの攻撃！的に15ポイントのダメージを与えた";
-		System.out.println(msg);
+		int a = 10;
+		int b = 10;
+		System.out.println(++a + 50);
+		System.out.println(b++ + 50);
 	}
 }
