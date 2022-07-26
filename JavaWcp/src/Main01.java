f478fd31061450e12ca13e7ec6825279af3eb785
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-26 21:57:44 +0900

    [add]練習

---
+++ JavaWcp/src/Main01.java
@@ -0,0 +1,20 @@
+
+public class Main01 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅3横幅3の長方形の面積は" + c);
+		
+		boolean result = true;
+		System.out.println(result);
+		char favoriteCharacter = '駆';
+		System.out.println(favoriteCharacter);
+		double pi = 3.14;
+		System.out.println(pi);
+		long number = 3141592655853979L;
+		System.out.println(number);
+		String msg = "ミナトの攻撃！的に15ポイントのダメージを与えた";
+		System.out.println(msg);
+	}
+}
