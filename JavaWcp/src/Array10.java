18c22617d6c116153d479a02d6f47f11276d9788
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-27 21:48:15 +0900

    [add]配列学習

---
+++ JavaWcp/src/Array10.java
@@ -0,0 +1,12 @@
+
+public class Array10 {
+	public static void main(String[] args) {
+		int[] moneyList = {121901, 8302, 55100};
+		for (int i = 0; i < moneyList.length ; i++ ) {
+			System.out.println(moneyList[i]);
+		}
+		for (int m : moneyList) {
+			System.out.println(m);
+		}
+	}
+}
